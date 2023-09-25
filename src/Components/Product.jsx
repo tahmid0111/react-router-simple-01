860b2e18a0372254ce10ff494d43cfac7568a72f
tahmid-emam <0.1tahmidemam@gmail.com>
2023-09-25 20:31:15 +0600

    border radious added to show details link

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -10,8 +10,8 @@ const Product = (props) => {
           <h1 className='text-yellow-600 text-2xl pb-10'>{title}</h1>
         </Link>
         <h4 className='pb-5 text-justify'>{description}</h4>
-        <h2>{price} $</h2>
-        <Link to={`/product/${id}`} className='text-2xl'>show details</Link>
+        <h2 className='text-3xl text-green-500 pb-5'>{price} $</h2>
+        <Link to={`/product/${id}`} className='text-center text-md border-2 border-red-500 rounded-lg p-2'>show details</Link>
       </div>
     </>
   )
